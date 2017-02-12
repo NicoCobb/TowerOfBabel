959abdb8a3997c98d5a9075ad107373061b8802c
Dylan Tai <dylantai@union-wl-333.dynamic2.rpi.edu>
2017-02-12 01:29:53 -0500

    Fixed level, started LevelEditor, still gay

---
+++ Assets/Scripts/LevelManagerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LevelManagerScript : MonoBehaviour {
+
+	[HideInInspector]
+	public int[] levels;
+	public int totalLevels = 1;
+
+	// Use this for initialization
+	void Start () {
+		levels = new int[1,2,3,4];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
